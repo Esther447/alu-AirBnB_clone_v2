9e33be6330d3a152236d930daccd1a4de3f8cb86
Esther447 <e.mushimiyi@alustudent.com>
2025-05-27 21:42:11 +0200

    worked

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,37 @@
 #!/usr/bin/env bash
-# script that sets up web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+# Sets up web servers for deployment of web_static
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
+# Install nginx if not installed
+sudo apt-get update -y
+sudo apt-get install -y nginx
+
+# Create directories
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
+sudo mkdir -p /data/web_static/shared/
+
+# Create fake HTML file
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+# Recreate symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu:ubuntu /data/
+# Give ownership to ubuntu user and group
+sudo chown -R $(whoami):$(id -gn) /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+# Update Nginx configuration to serve content
+nginx_conf="/etc/nginx/sites-available/default"
+if ! grep -q "location /hbnb_static" "$nginx_conf"; then
+  sudo sed -i "/listen 80 default_server;/a \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" "$nginx_conf"
+fi
 
+# Restart Nginx
 sudo service nginx restart
+
+exit 0
+
