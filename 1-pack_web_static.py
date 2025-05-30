886db94a42b02419307f92423337e651d7fbda29
Esther447 <e.mushimiyi@alustudent.com>
2025-05-30 12:23:32 +0200

    worked

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-"""Creates a .tgz archive from web_static folder."""
+"""
+Fabric script to genereate tgz archive
+execute: fab -f 1-pack_web_static.py do_pack
+"""
 
-from fabric.api import local
 from datetime import datetime
-import os
+from fabric.api import *
+
 
 def do_pack():
-    """Generate a .tgz archive from web_static folder.
-
-    Returns:
-        str: Archive path if success, None otherwise.
     """
-    try:
-        if not os.path.exists("versions"):
-            os.makedirs("versions")
+    making an archive on web_static folder
+    """
 
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        archive_name = f"web_static_{timestamp}.tgz"
-        archive_path = f"versions/{archive_name}"
-
-        print(f"Packing web_static to {archive_path}")
-        result = local(f"tar -cvzf {archive_path} web_static", capture=True)
-        if result.succeeded:
-            print(f"web_static packed: {archive_path}")
-            return archive_path
-        return None
-
-    except Exception:
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
         return None
