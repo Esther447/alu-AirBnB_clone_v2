9c8710d648a935f2bd5357a1abb826c0393ba894
Esther447 <e.mushimiyi@alustudent.com>
2025-06-03 19:43:42 +0200

    done

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
+""" Starts a Flask web application with multiple routes """
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def hello():
+    """Returns a greeting"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Returns 'HBNB'"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """Displays 'C ' followed by the value of the text variable,
+    replacing underscores with spaces
+    """
     return "C {}".format(text.replace('_', ' '))
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
